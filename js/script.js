1df28df0cf35a1a2f0038f7fff84347fb7ac237b
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-02 22:46:02 -0500

    Beginning setup
    Beginning setup up of a weight calculator.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+// Jarrod Smith
+// SDI Section 1 
+// Expressions
+
+//Variables
+
+var weightSq = 40.84;      //Weight of 1 inch steel plate
+var thickness = 0;         //Thickness of the steel plate
+var length = 0;            //Length of steel plate
+var width = 0;             //Width of steel plate
+var sqInches = 0;          //Total sq inches of steel plate
+var adjSqWeight = 0;       //Weight adjust for the inputed plate weight
+
+//Inputs
+
+
+//Outputs
+
