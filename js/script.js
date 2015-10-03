f00b9d9d5b4c1ae437bffe09e0951bbc1547f661
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-02 23:22:03 -0500

    Equations
    Equations have been added to figure for output.

--- js/script.js
+++ js/script.js
@@ -10,14 +10,22 @@ var weightSq = 40.84;      //Weight of 1 inch steel plate
 var thickness = 0;         //Thickness of the steel plate
 var length = 0;            //Length of steel plate
 var width = 0;             //Width of steel plate
-var sqInches = 0;          //Total sq inches of steel plate
+var sqFeet = 0;            //Total sq feet of steel plate
 var adjSqWeight = 0;       //Weight adjust for the inputed plate weight
+var total = 0;
 
 //Inputs
 
 var thickness = prompt("Enter the thickness of the steel plate in deciaml.");   //User inputs plate thickness
 var length = prompt ("Enter the length of your plate in inches.");              //User inputs plate length in inches
 var width = prompt ("Enter the width of your plate in inches");                 //User inputs plate width in inches
+
+//Equations
+
+var adjSqWeight = weightSq * thickness;
+var sqFeet = length * width / 144;
+var total = adjSqWeight * sqFeet;
+
 
 //Outputs
 
