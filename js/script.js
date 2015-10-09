3da8f7b4054d4e2643e30759216e0ed7ad4e1ef5
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-08 20:27:47 -0500

    Comments and test
    Added comments and after a couple of more test to ensure th output is
    accurate.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // JavaScript Document
-
 // Jarrod Smith
 // SDI Section 1 
 // Expression assignment
@@ -7,14 +6,14 @@
 //Variables
 
 var weightSq = 40.84;       //Weight of 1 inch steel plate
-var thickness;          //Thickness of the steel plate
-var length;             //Length of steel plate
-var width;              //Width of steel plate
-var sqFeet;             //Total sq feet of steel plate
-var adjSqWeight;        //Weight adjust for the inputed plate weight
-var total;              //The total weight for plate
-var price;              //The price you are paying per pound.
-var cost;               //Total cost for plate
+var thickness;              //Thickness of the steel plate
+var length;                 //Length of steel plate
+var width;                  //Width of steel plate
+var sqFeet;                 //Total sq feet of steel plate
+var adjSqWeight;            //Weight adjust for the inputed plate weight
+var total;                  //The total weight for plate
+var price;                  //The price you are paying per pound.
+var cost;                   //Total cost for plate
 
 //Inputs
 
@@ -34,3 +33,13 @@ var cost = total * price;                           //Equating the cost for the 
 
 
 alert ("The total square feet of the plate is " + sqFeet + ".\nThe total weight of the plate is " + total + ".\nThe total cost for the plate is "+ cost + ".");
+
+//Test
+
+// I entered in .5 for the thickness prompt, for the length I entered 24, for the width I entered 24,
+// for price per pound I entered .50. My output is 4 sq feet, 81.68 total weight, and the cost for the $40.84
+// Manually figuring the problem I came up with the same thing.
+
+// I entered in .75 for the thickness prompt, for the length I entered 59, for the width I entered 94,
+// for the price per pound I entered .45. My output is 38.513 sq feet, 1179.6804 total weight, and the cost of 530.856
+// Manually figuring the problem I came up with the same thing.
